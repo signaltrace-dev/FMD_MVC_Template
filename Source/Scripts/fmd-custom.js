a5a7ab8a7e987828f670f6824dffdb683895aeea
Jeff Glenn <jglenn@uga.edu>
2013-05-22 15:02:16 +0000

    git-svn-id: https://svn.fmd.uga.edu/projects/FMD_MVC_Template@1 2c3d12fa-70d1-415a-932a-b265f8d4dda1

---
+++ Source/Scripts/fmd-custom.js
@@ -0,0 +1,34 @@
+﻿function disable(button) {
+    Page_ClientValidate();
+    if (Page_IsValid) { 
+        $(button).attr('disabled', 'disabled');
+    }
+}
+
+function enable(button) {
+    $(button).attr('disabled', false);
+}
+
+/*
+$(window).load(function(){
+	if ($(".flexslider").length > 0){
+	    $(".flexslider").flexslider({
+	        animation: "slide",
+	    });
+	}
+});
+*/
+
+$(document).ready(function() {
+    $(".alert-fade").fadeOut(5000, function(){});
+    
+});
+
+$(document).ready(function() {
+	if ($(".highlight").length > 0){
+		$(".highlight").effect('highlight', {}, 3000);
+	}
+});
+
+
+
